8390ffd1b6a189b4f50ef95a628ddf90368595fc
Victoria Moya Anna Ceccaroni <victoriaceccaroni@gmail.com>
2024-04-12 08:59:23 +0200

    assembly quine 1 finished - adding explanations

---
+++ first_quine/quine.asm
@@ -0,0 +1,21 @@
+section .data
+    ; This is a comment outside of the program
+    msg db "section .data%c    msg db %c%s%c, 10, 34, 34, 10", 10, 34, 34, 10 ; Comment inside the program
+
+
+section .text
+    global _start
+
+_start:
+   ; This is a comment in the main function
+    mov rax, 1                  ; syscall number for sys_write
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    lea rsi, [msg]              ; address of msg
+    mov rdx, msg.len            ; message length
+    syscall                     ; invoke syscall
+
+    mov rax, 60                 ; syscall number for sys_exit
+    xor rdi, rdi                ; exit status 0
+    syscall                     ; invoke syscall
+
+msg.len equ $ - msg             ; calculate the length of the message
