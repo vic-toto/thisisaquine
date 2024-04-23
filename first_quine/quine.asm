70023b0c9baadcbbb7c68b1192b089490b2e8726
Victoria Moya Anna Ceccaroni <victoriaceccaroni@gmail.com>
2024-04-23 18:06:43 +0200

    created Makefile but it's broken

--- first_quine/quine.asm
+++ first_quine/quine.asm
@@ -1,7 +1,8 @@
 section .data
     ; This is a comment outside of the program
-    msg db "section .data%c    msg db %c%s%c, 10, 34, 34, 10", 10, 34, 34, 10 ; Comment inside the program
-
+    msg db "section .data%c    msg db %c%s%c, 10, 34, 34, 10", 10, 34, 34, 10 
+    ; Comment inside the program
+    msg_len equ $ - msg             ; calculate the length of the message 
 
 section .text
     global _start
@@ -11,11 +12,9 @@ _start:
     mov rax, 1                  ; syscall number for sys_write
     mov rdi, 1                  ; file descriptor 1 (stdout)
     lea rsi, [msg]              ; address of msg
-    mov rdx, msg.len            ; message length
+    mov rdx, msg_len            ; message length
     syscall                     ; invoke syscall
 
     mov rax, 60                 ; syscall number for sys_exit
     xor rdi, rdi                ; exit status 0
     syscall                     ; invoke syscall
-
-msg.len equ $ - msg             ; calculate the length of the message
