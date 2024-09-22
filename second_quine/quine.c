73568aae22a65d427d4175a86cd09e1cd7579df9
vic-toto <victoriaceccaroni@gmail.com>
2024-09-22 21:28:40 +0200

    working on second quine still

--- second_quine/quine.c
+++ second_quine/quine.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 #define OPEN_FILE FILE *fd = fopen("Grace_kid.c", "w");
-#define CODE "#include <stdio.h>%1$c%1$c#define OPEN_FILE FILE *fd = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);%1$c#define CODE %2$c%3$s%2$c%1$c#define WRITE_CODE fprintf(fd, CODE, 10, 34, CODE, 34); fclose(fd);%1$c/* This is a quine */%1$c__attribute__((constructor)) void init() { OPEN_FILE; WRITE_CODE; }%1$c"
-#define WRITE_CODE fprintf(fd, CODE, 10, 34, CODE); fclose(fd);
+#define CODE "#include <stdio.h>%1$c%1$c#define OPEN_FILE FILE *fd = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c);%1$c#define CODE %2$c%3$s%2$c%1$c#define WRITE_CODE OPEN_FILE; fprintf(fd, CODE, %4$d, %4$d, CODE, %4$d); fclose(fd);%1$c%1$c/* This is a quine */%1$c%1$cWRITE_CODE%1$c %1$c"
+#define WRITE_CODE OPEN_FILE; fprintf(fd, CODE, 10, 16, 52, 34, CODE, 34); fclose(fd);
 
 /* This is a quine */
-__attribute__((constructor))
-void init() {
-    OPEN_FILE;
-    WRITE_CODE;
-}
+
+WRITE_CODE
+
 
 // %1$c: Represents a newline character (\n). 
 // It's commonly used to insert line breaks in the source code.
