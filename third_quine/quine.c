b2837fcd0948dc8b74c9291ff269d37ffa7c5033
vic-toto <victoriaceccaroni@gmail.com>
2024-09-29 19:52:13 +0200

    working third quine, need to fix program repetition bug

--- third_quine/quine.c
+++ third_quine/quine.c
@@ -2,20 +2,16 @@
 
 int num = 5; 
 
-#define CODE "#include <stdio.h>%1$c%1$cint num = %2$d;%1$c#define CODE %2$c%3$s%2$c%1$c%1$cint main() {%1$c    if (num > 0) {%1$c        num--; %1$c        char filename[20];%1$c        sprintf(filename, %2$cSully_%d.c%2$c, num);%1$c        FILE *fd = fopen(filename, %2$cw%2$c);%1$c        fprintf(fd, CODE, 10, 34, CODE, 34);%1$c        fclose(fd);%1$c    }%1$c    return (0);%1$c}%1$c"
+#define CODE "#include <stdio.h>%1$c%1$cint num = 5;%1$c%1$c#define CODE %2$c%3$s%2$c%1$c%1$cint main() {%1$c    if (num >= 0) {%1$c        char filename[20];%1$c        sprintf(filename, %2$cSully_%%d.c%2$c, num);%1$c        FILE *fd = fopen(filename, %2$cw%2$c);%1$c        fprintf(fd, CODE, 10, 34, num);%1$c        fclose(fd);%1$c        num--;%1$c    }%1$c    return (0);%1$c}%1$c"
 
 int main() {
-    if (num >= 0) {
-        num--;
+    if (num >= 0) { 
         char filename[20];
         sprintf(filename, "Sully_%d.c", num);
         FILE *fd = fopen(filename, "w");
-        if (fd) {
-            fprintf(fd, CODE, 10, 34, num, 34, CODE);
-            fclose(fd);
-        } else {
-            perror("Error opening file");
-        }
+        fprintf(fd, CODE, 10, 34, CODE);
+        fclose(fd);
+        num--;
     }
     return 0;
 }
