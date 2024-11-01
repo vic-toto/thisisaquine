4743526976411dd3b2b3c6f367a1c40c3352355c
vic-toto <victoriaceccaroni@gmail.com>
2024-11-01 15:34:05 +0100

    fixed Grace quine

--- Grace/quine.c
+++ Grace/quine.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-#define CODE "#include <stdio.h>%1$c%1$c#define CODE %2$c%3$s%2$c%1$c#define WRITE_FILE FILE *fd = fopen(%4$cGrace_kid.c%4$c, %4$cw%4$c); fprintf(fd, CODE, 10, 34, CODE, 34); fclose(fd)%1$c%1$c/* This is a quine */%1$c%1$c#define EXECUTE() int main (void){WRITE_FILE; return 0;}%1$c%1$cEXECUTE()%1$c"
-#define WRITE_FILE FILE *fd = fopen("Grace_kid.c", "w"); fprintf(fd, CODE, 10, 34, CODE, 34); fclose(fd)
+#define CODE "#include <stdio.h>%1$c%1$c#define CODE %2$c%3$s%2$c%1$c#define WRITE_FILE FILE *fd = fopen(%2$cGrace_kid.c%2$c, %2$cw%2$c); fprintf(fd, CODE, 10, 34, CODE); fclose(fd)%1$c%1$c/*%1$c    This is a useless comment that made me fail last time%1$c*/%1$c%1$c#define EXECUTE() int main (void){WRITE_FILE; return 0;}%1$c%1$cEXECUTE()"
+#define WRITE_FILE FILE *fd = fopen("Grace_kid.c", "w"); fprintf(fd, CODE, 10, 34, CODE); fclose(fd)
 
-/* This is a quine */
+/*
+    This is a useless comment that made me fail last time
+*/
 
 #define EXECUTE() int main (void){WRITE_FILE; return 0;}
 
